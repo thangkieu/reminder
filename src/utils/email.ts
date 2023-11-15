77c131f1d736c5362116edb94b94f93e873dcb37
Thang Kieu <kqthang1505@gmail.com>
2023-11-15 14:04:13 +0800

    Logging

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -3,8 +3,6 @@ import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 import type Mail from 'nodemailer/lib/mailer';
 function getTransporter() {
-  console.debug('Email Server', process.env.EMAIL_USER, process.env.EMAIL_PASSWORD);
-
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -18,6 +16,16 @@ function getTransporter() {
 
 export async function sendEmail(payload: Mail.Options) {
   const transporter = getTransporter();
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error: any, success: any) {
+      if (success) resolve(success);
+
+      console.log('Verify Transporter failed', error);
+      reject(error);
+    });
+  });
 
   // send mail with defined transport object
 
